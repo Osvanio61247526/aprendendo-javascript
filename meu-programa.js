b4bec0267a309a11425445bd8920b2f6a484894f
Osvanio Jose de Souza <osvanio61247526@edu.df.senac.br>
2026-05-06 17:45:38 -0300

    trabalhando no script

---
+++ meu-programa.js
@@ -0,0 +1,7 @@
+ /*let nome = window.prompt( "digite seu nome")
+let anoNacimento = window.prompt("digite o ano do seu nacimento")
+let idade = 2026-anoNacimento
+window. alert ( "seu nome é " + nome + " e voce tem " + idade + " anos " )
+*/
+
+
---
+++ script.js
@@ -0,0 +1,15 @@
+window.alert("olá mundo!")
+
+window.confirm('aprendendo javascrpt')
+let nomeAluno = window.prompt( 'qual é seu nome?')
+window.alert(nomeAluno)
+window.alert(1000)
+
+
+var nomePessoa = "maria"
+let idadeDaPessoa = 20
+let turmaSenacTecnicoInformaticaGama = '20261035'
+let informatica = 20261035 // popde ser um ( int/integer  ( inteiro))
+let peso = 54.5 //float ( numeros reais ( ponto flutuante/real. numeros reais))
+const TEM_AULA_SEXTA = false // 0  (verdadeiro)
+let temAulaAmanha = true // 1  (falso)
